3fd767ce1aaf848b6178cc37d968609d9588d403
MercioGiaretta <128699194+MercioGiaretta@users.noreply.github.com>
2023-09-05 12:02:33 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var num1= 35;
+var num2= 15;
+
+//console.log (num1+num2);
+
+var soma;
+var resultado;
+
+function calcularSoma(num1, num2) {
+  soma = num1 + num2;
+  return soma; 
+}
+
+resultado = calcularSoma(num1, num2);
+console.log('ta certo a soma:',resultado);
+
+num1 = 36
+num2 = 14
+
+resultado = calcularSoma(num1,num2);
+console.log('A soma de ',num1, 'e',num2, 'é', resultado);
